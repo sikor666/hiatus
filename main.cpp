876341525ee3f02f15744e8b2a21564f4d3f6d4e
Tomek <sikor666@gmail.com>
2018-10-25 22:50:55 +0200

    Add template function

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <deque>
 #include <iostream>
 #include <vector>
 
@@ -14,10 +15,17 @@ struct Point
 template<typename T> class Vector
 {
 public:
-	T& operator[](std::size_t index)
+	T operator[](std::size_t index)
 	{
+		return T{};
 	}
 };
+
+template<typename Container, typename Index>
+auto access(Container& c, Index i) -> decltype(c[i])
+{
+	return c[i];
+}
 
 int main(int argc, char **argv)
 {
@@ -25,7 +33,7 @@ int main(int argc, char **argv)
 	std::for_each(argv, argv + argc, print);
 
 	const int i = 0; // decltype(i) to const int
-	decltype(i) z = 1;
+	decltype(i) z = i;
 	std::cout << "decltype(i): " << typeid(decltype(i)).name() << std::endl;
 	std::cout << "decltype(z): " << typeid(decltype(z)).name() << std::endl;
 
@@ -40,6 +48,13 @@ int main(int argc, char **argv)
 	Vector<decltype(p)> v;
 	std::cout << "decltype(v): " << typeid(decltype(v)).name() << std::endl;
 	std::cout << "decltype(Vector::operator): " << typeid(decltype(&Vector<decltype(p)>::operator[])).name() << std::endl;
+
+	auto r = access(v, z);
+	std::cout << "decltype(r): " << typeid(decltype(r)).name() << std::endl;
+
+	std::deque<int> d {0};
+	access(d, z) = 1;
+	std::cout << "decltype(authAndAccess(d, z)): " << typeid(decltype(access<decltype(d), decltype(z)>)).name() << std::endl;
 
 	return 0;
 }
